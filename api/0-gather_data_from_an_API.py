bebd144cf23610a42b547110ae5a4443934461d3
Guled12345 <g.warsameh@alustudent.com>
2023-07-09 07:32:51 -0700

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,54 @@
 #!/usr/bin/python3
-"""Python script that returns the value of an employee's TODO list"""
+"""
+    python script that returns TODO list progress for a given employee ID
+"""
 import json
-import sys
-import urllib.request
+import requests
+from sys import argv
 
-if __name__ == '__main__':
-    """Converting the arguments into integers"""
-    EMPLOYEE_ID = int(sys.argv[1])
 
-    """Using URL to retrieve data about employees"""
-    BASE_URL = "https://jsonplaceholder.typicode.com"
-    employee_url = "{}/users/{}".format(BASE_URL, EMPLOYEE_ID)
-    todo_url = "{}/todos?userId={}".format(BASE_URL, EMPLOYEE_ID)
+if _name_ == "_main_":
+    """
+        request user info by employee ID
+    """
+    request_employee = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
+    """
+        convert json to dictionary
+    """
+    employee = json.loads(request_employee.text)
+    """
+        extract employee name
+    """
+    employee_name = employee.get("name")
 
-    """Opening employee URL using urllib module"""
-    with urllib.request.urlopen(employee_url) as f:
-        employee_info = json.loads(f.read().decode('utf-8'))
+    """
+        request user's TODO list
+    """
+    request_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+    """
+        dictionary to store task status in boolean format
+    """
+    tasks = {}
+    """
+        convert json to list of dictionaries
+    """
+    employee_todos = json.loads(request_todos.text)
+    """
+        loop through dictionary & get completed tasks
+    """
+    for dictionary in employee_todos:
+        tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
-    with urllib.request.urlopen(todo_url) as response:
-        data = json.loads(response.read().decode('utf-8'))
-
-    """Retrieving data about employee name, completed tasks, and total number of tasks"""
-    EMPLOYEE_NAME = employee_info["name"]
-    TOTAL_NUMBER_OF_TASKS = len(data)
-    NUMBER_OF_DONE_TASKS = len([task for task in data if task["completed"]])
-
-    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-
-    for task in data:
-        if task["completed"]:
-            print("\t{}".format(task["title"]))
+    """
+        return name, total number of tasks & completed tasks
+    """
+    EMPLOYEE_NAME = employee_name
+    TOTAL_NUMBER_OF_TASKS = len(tasks)
+    NUMBER_OF_DONE_TASKS = len([k for k, v in tasks.items() if v is True])
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for k, v in tasks.items():
+        if v is True:
+            print("\t {}".format(k))
